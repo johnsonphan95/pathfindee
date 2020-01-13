0a241379889c9e4b7959403678f56c81ef59a37c
Johnson <johnsonphan95@gmail.com>
2020-01-13 11:08:34 -0800

    Add footer and icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
 import PathFinder from "./components/PathFinder";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithubSquare } from "@fortawesome/free-brands-svg-icons";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <PathFinder />
+      <footer>
+        <a href="http://linkedin.com/in/johnsonphan95">
+          <FontAwesomeIcon id="icon" icon={faLinkedin}></FontAwesomeIcon>
+        </a>
+        <a href="http://github.com/johnsonphan95">
+          <FontAwesomeIcon id="icon" icon={faGithubSquare}></FontAwesomeIcon>
+        </a>
+        <div>
+          TO ADD WALLS OR WEIGHTED NODES JUST LEFT CLICK AND DRAG
+          <br />
+          <br />
+          TO MOVE THE START OR END NODE JUST LEFT CLICK AND MOVE
+        </div>
+      </footer>
     </div>
   );
 }
